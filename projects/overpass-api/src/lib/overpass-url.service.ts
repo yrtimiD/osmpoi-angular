58aef40bd38ceac9a16bb57e46e6664a31f166de
yrtimiD <yrtimid@gmail.com>
2019-11-23 22:35:03 +0200

    more overpass services + random selection

--- projects/overpass-api/src/lib/overpass-url.service.ts
+++ projects/overpass-api/src/lib/overpass-url.service.ts
@@ -3,13 +3,16 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class OverpassUrlService {
   private servers: string[] = [
+    "https://overpass-api.de/api/interpreter",
     "https://overpass.kumi.systems/api/interpreter",
-    "https://overpass-api.de/api/interpreter"
+    "http://overpass.openstreetmap.fr/api/interpreter",
+    // "https://overpass.nchc.org.tw"
   ];
 
   constructor() { }
 
   public getInterpreterUrl(): string {
-    return this.servers[0]; //TODO: add some smart check/round-robin logic here
+    const index = Math.floor(Math.random() * this.servers.length);
+    return this.servers[index];
   }
 }

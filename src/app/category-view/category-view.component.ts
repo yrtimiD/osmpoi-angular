abec8ce9538ec28c55423ac0bfe8485a61142dbf
yrtimiD <yrtimid@gmail.com>
2019-11-23 22:25:35 +0200

    Showing poi under their category (1 level)

--- src/app/category-view/category-view.component.ts
+++ src/app/category-view/category-view.component.ts
@@ -2,20 +2,25 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { NGXLogger } from 'ngx-logger';
 import { PoiService, Poi } from '@osmpoi/poi';
 
+interface PoisByCategory { [category: string]: Poi[]; }
+
 @Component({
   selector: 'yr-category-view',
   templateUrl: './category-view.component.html',
   styleUrls: ['./category-view.component.scss']
 })
 export class CategoryViewComponent implements OnInit {
-  public pois: Poi[];
+  public poisByCategory: PoisByCategory;
+
   @Output() poiSelected = new EventEmitter<Poi>();
 
   constructor(private log: NGXLogger, private poiService: PoiService) {
   }
 
   ngOnInit() {
-    this.pois = this.poiService.getPois();
+    this.poiService.getPois().then((pois) => {
+      this.poisByCategory = this.buildPoiTree(pois);
+    });
   }
 
   public onSelect(poi: Poi): void {
@@ -23,4 +28,16 @@ export class CategoryViewComponent implements OnInit {
     this.poiSelected.emit(poi);
   }
 
+  private buildPoiTree(pois: Poi[]): PoisByCategory {
+    const tree: PoisByCategory = {};
+
+    for (let poi of pois) {
+      for (let category of poi.categories) {
+        let cat = tree[category] = tree[category] || [];
+        cat.push(poi);
+      }
+    }
+
+    return tree;
+  }
 }

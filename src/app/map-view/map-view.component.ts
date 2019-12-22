ce64aecb34d8f2c712470fe1ef1dbca3338b07cc
yrtimiD <yrtimid@gmail.com>
2019-12-23 01:39:55 +0200

    better map attribution

--- src/app/map-view/map-view.component.ts
+++ src/app/map-view/map-view.component.ts
@@ -17,7 +17,11 @@ export class MapViewComponent implements OnInit {
   ngOnInit() {
     this.options = {
       layers: [
-        tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '© OpenStreetMap contributors' })
+        tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          {
+            maxZoom: 18,
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+          })
       ],
       zoom: 14,
       center: latLng(this.centerLat, this.centerLon),
